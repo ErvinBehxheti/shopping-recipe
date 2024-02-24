2cfc4a00a560ef1ba5bbf9830a99f5c14e57e952
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-02-24 14:03:19 +0100

    recipe list added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Burek',
+      'Burek me mish',
+      'https://i.ytimg.com/vi/JKvvXGcBKJ4/maxresdefault.jpg'
+    ),
+  ];
 }
