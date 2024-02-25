9e70e4ee265c00a771e54f973f1324cc8dfdeb19
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-02-25 22:40:15 +0100

    add to shopping list and details for recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
     new Recipe(
       'Burek',
       'Burek me mish',
       'https://i.ytimg.com/vi/JKvvXGcBKJ4/maxresdefault.jpg'
     ),
+    new Recipe(
+      'Burek1',
+      'Burek me mish1',
+      'https://i.ytimg.com/vi/JKvvXGcBKJ4/maxresdefault.jpg'
+    ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,11 +4,15 @@ import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     new Ingredient('Mish', 5),
-    new Ingredient('Mill', 10)
+    new Ingredient('Mill', 10),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
